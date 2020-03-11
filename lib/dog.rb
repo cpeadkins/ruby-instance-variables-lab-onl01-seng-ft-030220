dc628b3c8522373cf2497afe9caa3753e9ec1444
Charles Adkins <cpeadkins@gmail.com>
2020-03-11 14:56:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,11 +6,11 @@ class Dog
   def name
     this_dogs_name
   end
-  
-  lassie = Dog.new
-lassie.name = "Lassie"
- 
-lassie.name #=> "Lassie"
+
   
 end
 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+lassie.name #=> "Lassie"
